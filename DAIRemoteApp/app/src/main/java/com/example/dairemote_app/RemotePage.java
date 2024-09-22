e53119d94ce55c62f088f517945907b0bea95ea7
Mehad Ali <mehad.ali760@gmail.com>
2024-09-21 22:50:48 -0400

    Uploaded icons for features of app. Created Activity pages for features and implemented button navigation back and forth from menu button page to other pages (about, remote, instructions)

--- DAIRemoteApp/app/src/main/java/com/example/dairemote_app/RemotePage.java
+++ DAIRemoteApp/app/src/main/java/com/example/dairemote_app/RemotePage.java
@@ -8,13 +8,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class InteractionPage extends AppCompatActivity {
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+public class RemotePage extends AppCompatActivity {
+
+    Button homePage_button; // button for returning to home page (menu buttons)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_interaction_page);
+        setContentView(R.layout.activity_remote_page);
+
+        homePage_button = findViewById(R.id.homeButton);
+        homePage_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RemotePage.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
