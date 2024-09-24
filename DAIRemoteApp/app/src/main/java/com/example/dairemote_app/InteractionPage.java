00950488f76d0859996d71c2d6dc815e0fe71233
Mehad Ali <fj2852@wayne.edu>
2024-09-24 15:03:40 -0400

    Revert "Added icons, activity pages, and navigation for app features; adjusted layout dimensions for responsiveness."

--- DAIRemoteApp/app/src/main/java/com/example/dairemote_app/InteractionPage.java
+++ DAIRemoteApp/app/src/main/java/com/example/dairemote_app/InteractionPage.java
@@ -8,29 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import android.view.View;
-import android.widget.Button;
-import android.content.Intent;
-
-public class RemotePage extends AppCompatActivity {
-
-    Button homePage_button; // button for returning to home page (menu buttons)
+public class InteractionPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_remote_page);
-
-        homePage_button = findViewById(R.id.homeButton);
-        homePage_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(RemotePage.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
-
+        setContentView(R.layout.activity_interaction_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
