ae587799b9e50109c65d38fe6066b035e76864ba
Fahim-zzz <fahimzaman@wayne.edu>
2024-09-29 17:21:30 -0400

    Implemented UDP server for host and client communication. Updated app interactions page to send host interaction information. Implemented initialization of server on launch of application. (#46)
    * Implemented UDP server for host and client communication.
    * Updated app interactions page to send host interaction information.
    * Implemented initialization of server on launch of desktop application.

---
+++ DAIRemote/UDPServer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace DAIRemote
+{
+    class UDPServer
+    {
+        public void hostUDPServer()
+        {
+            UdpClient udpServer = new UdpClient(11000);
+            IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 11000);
+
+            try
+            {
+                Debug.WriteLine("UDP Server is listening...");
+
+                while (true)
+                {
+                    // Receive data from client
+                    byte[] data = udpServer.Receive(ref remoteEP);
+                    string receivedData = Encoding.ASCII.GetString(data);
+                    Debug.WriteLine($"Received: {receivedData}");
+
+                    /*// Send a response back to the client
+                    string responseData = "Data received!";
+                    byte[] responseBytes = Encoding.ASCII.GetBytes(responseData);
+                    udpServer.Send(responseBytes, responseBytes.Length, remoteEP);
+                    Debug.WriteLine("Sent response back to client");
+                    */
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.ToString());
+            }
+            finally
+            {
+                udpServer.Close();
+            }
+        }
+    }
+}
