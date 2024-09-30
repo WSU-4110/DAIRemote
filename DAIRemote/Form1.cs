fac5dbcd7823b769a45772fb1054a1e67379220e
shawiminhas <shawiminhas@hotmail.com>
2024-09-30 02:53:40 -0400

    Add functionality for hiding audio output form and fix white background bug

--- DAIRemote/Form1.cs
+++ DAIRemote/Form1.cs
@@ -8,6 +8,7 @@ namespace DAIRemote
     public partial class Form1 : System.Windows.Forms.Form
     {
         private TrayIconManager trayIconManager;
+        private AudioOutputForm audioForm;
         private Panel audioFormPanel;
 
         public Form1()
@@ -29,44 +30,52 @@ namespace DAIRemote
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.Hide(); 
+            this.Hide();
         }
 
         private void BtnShowAudioOutputs_Click(object sender, EventArgs e)
         {
-            audioFormPanel.Controls.Clear();
-
-            AudioOutputForm audioForm = new AudioOutputForm
+            if (audioForm == null || audioForm.IsDisposed)
             {
-                TopLevel = false,
-                FormBorderStyle = FormBorderStyle.None,
-                Dock = DockStyle.Fill 
-            };
+                audioForm = new AudioOutputForm
+                {
+                    TopLevel = false,
+                    FormBorderStyle = FormBorderStyle.None,
+                    Dock = DockStyle.Fill
+                };
 
-            audioFormPanel.Controls.Add(audioForm); 
-            audioForm.Show(); 
+                audioFormPanel.Controls.Add(audioForm);
+                audioForm.Show();
+            }
+            else
+            {
+                if (audioForm.Visible)
+                {
+                    audioForm.Hide();
+                }
+                else
+                {
+                    audioForm.Show();
+                }
+            }
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            if (e.CloseReason == CloseReason.UserClosing)
-            {
-                e.Cancel = false; 
-                trayIconManager.HideIcon();
-            }
+            trayIconManager.HideIcon();
         }
 
         private void InitializeCustomComponents()
         {
             this.audioFormPanel = new Panel
             {
-                Location = new System.Drawing.Point(10, 60), 
-                Size = new System.Drawing.Size(760, 370), 
+                Location = new System.Drawing.Point(10, 60),
+                Size = new System.Drawing.Size(760, 370),
             };
 
             this.Controls.Add(this.audioFormPanel);
         }
-       
+
         private void BtnSaveDisplayConfig_Click(object sender, EventArgs e)
         {
             string fileName = "displayConfig";
