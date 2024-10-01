217c9880a31836990022978f4a582cfb739ae026
Fahim-zzz <fahimzaman@wayne.edu>
2024-09-30 23:21:24 -0400

    Centered window on launch and added text box for display profile name

--- DAIRemote/Form1.designer.cs
+++ DAIRemote/Form1.designer.cs
@@ -18,46 +18,80 @@
 
         #region Windows Form Designer generated code
 
-        
+
         private void InitializeComponent()
         {
             BtnSaveDisplayConfig = new Button();
-            BtnShowAudioOutputs = new Button(); 
+            BtnShowAudioOutputs = new Button();
+            button1 = new Button();
+            profileNameTextBox = new TextBox();
             SuspendLayout();
-
+            // 
+            // BtnSaveDisplayConfig
+            // 
             BtnSaveDisplayConfig.AccessibleName = "BtnSaveDisplayConfig";
-            BtnSaveDisplayConfig.Location = new Point(20, 10);
+            BtnSaveDisplayConfig.BackColor = Color.LightSkyBlue;
+            BtnSaveDisplayConfig.Location = new Point(20, 42);
             BtnSaveDisplayConfig.Name = "BtnSaveDisplayConfig";
-            BtnSaveDisplayConfig.Size = new Size(200, 40); 
+            BtnSaveDisplayConfig.Size = new Size(200, 40);
             BtnSaveDisplayConfig.TabIndex = 0;
             BtnSaveDisplayConfig.Text = "Save Display Config";
-            BtnSaveDisplayConfig.BackColor = Color.LightSkyBlue; 
+            BtnSaveDisplayConfig.UseVisualStyleBackColor = false;
             BtnSaveDisplayConfig.Click += BtnSaveDisplayConfig_Click;
-
+            // 
+            // BtnShowAudioOutputs
+            // 
             BtnShowAudioOutputs.AccessibleName = "BtnShowAudioOutputs";
-            BtnShowAudioOutputs.Location = new Point(20, 70); 
+            BtnShowAudioOutputs.BackColor = Color.LightSkyBlue;
+            BtnShowAudioOutputs.Location = new Point(20, 88);
             BtnShowAudioOutputs.Name = "BtnShowAudioOutputs";
-            BtnShowAudioOutputs.Size = new Size(200, 40); 
+            BtnShowAudioOutputs.Size = new Size(200, 40);
             BtnShowAudioOutputs.TabIndex = 1;
             BtnShowAudioOutputs.Text = "Show Audio Outputs";
-            BtnShowAudioOutputs.BackColor = Color.LightSkyBlue; 
+            BtnShowAudioOutputs.UseVisualStyleBackColor = false;
             BtnShowAudioOutputs.Click += BtnShowAudioOutputs_Click;
-
-           
+            // 
+            // button1
+            // 
+            button1.AccessibleName = "BtnLoadDisplayConfig";
+            button1.BackColor = Color.LightSkyBlue;
+            button1.Location = new Point(20, 398);
+            button1.Name = "button1";
+            button1.Size = new Size(200, 40);
+            button1.TabIndex = 2;
+            button1.Text = "Load Display Config";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += BtnLoadDisplayConfig_Click;
+            // 
+            // profileNameTextBox
+            // 
+            profileNameTextBox.Location = new Point(20, 12);
+            profileNameTextBox.Name = "profileNameTextBox";
+            profileNameTextBox.Size = new Size(200, 23);
+            profileNameTextBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(225, 246, 225);
             ClientSize = new Size(300, 450);
-            BackColor = Color.FromArgb(225, 246, 225); 
+            Controls.Add(profileNameTextBox);
+            Controls.Add(button1);
             Controls.Add(BtnSaveDisplayConfig);
-            Controls.Add(BtnShowAudioOutputs); 
+            Controls.Add(BtnShowAudioOutputs);
             Name = "Form1";
             Text = "DAIRemote";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button BtnSaveDisplayConfig;
-        private Button BtnShowAudioOutputs; 
+        private Button BtnShowAudioOutputs;
+        private Button button1;
+        private TextBox profileNameTextBox;
     }
 }
