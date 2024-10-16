aa3bd922d951104ee7da048132ed7db1b12156bd
Fahim-zzz <fahimzaman@wayne.edu>
2024-10-16 02:22:17 -0400

    Some redesigning for page 2 of keyboard toolbar to include images. Added some functionality to buttons, but some still WIP.

--- DAIRemoteApp/app/src/main/java/com/example/dairemote_app/InteractionPage.java
+++ DAIRemoteApp/app/src/main/java/com/example/dairemote_app/InteractionPage.java
@@ -242,12 +242,12 @@ public class InteractionPage extends AppCompatActivity implements NavigationView
                     if (cursorPosition > 0) {
                         editText.getText().delete(cursorPosition - 1, cursorPosition);
                     }
-                    if (!MainActivity.connectionManager.sendHostMessage("KEYBOARD_WRITE {BACKSPACE}")) {
+                    if (!MainActivity.connectionManager.sendHostMessage("KEYBOARD_WRITE {BS}")) {
                         startHome();
                     }
                     return true;
                 } else if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
-                    if (!MainActivity.connectionManager.sendHostMessage("KEYBOARD_WRITE {ENTER}")) {
+                    if (!MainActivity.connectionManager.sendHostMessage("KEYBOARD_WRITE {~}")) {
                         startHome();
                     }
                     toggleKeyboardToolbar(false);
@@ -346,9 +346,12 @@ public class InteractionPage extends AppCompatActivity implements NavigationView
     }
 
     public void extraToolbarOnClick(View view) {
-       String msg = "";
-       int viewID = view.getId();
-        if(viewID == R.id.winKey) {
+        String msg = "";
+        int viewID = view.getId();
+        if(viewID == R.id.moreOpt) {
+            // Place holder to do nothing,
+            // on click listener will be done instead and is setup
+        } else if(viewID == R.id.winKey) {
             msg = "WIN";
         } else if(viewID == R.id.fnKey) {
             msg = "FN";
@@ -358,35 +361,65 @@ public class InteractionPage extends AppCompatActivity implements NavigationView
             msg = "CTRL";
         } else if(viewID == R.id.shiftKey) {
             msg = "SHIFT";
-        } else if(viewID == R.id.moreOpt) {
-            msg = "MORE OPTIONS IMPLEMENT HANDLING FOR 2ND PAGE";
-        } else if(viewID == R.id.f1Key) {
-            msg = "{F1}";
-        } else if(viewID == R.id.f2Key) {
-            msg = "{F2}";
-        } else if(viewID == R.id.f3Key) {
-            msg = "{F3}";
-        } else if(viewID == R.id.f4Key) {
-            msg = "{F4}";
-        } else if(viewID == R.id.f5Key) {
-            msg = "{F5}";
-        } else if(viewID == R.id.f6Key) {
-            msg = "{F6}";
-        } else if(viewID == R.id.f7Key) {
-            msg = "{F7}";
-        } else if(viewID == R.id.f8Key) {
-            msg = "{F8}";
-        } else if(viewID == R.id.f9Key) {
-            msg = "{F9}";
-        } else if(viewID == R.id.f10Key) {
-            msg = "{F10}";
-        } else if(viewID == R.id.f11Key) {
-            msg = "{F11}";
-        } else if(viewID == R.id.f12Key) {
-            msg = "{F12}";
+        } else {
+            if (currentPageIndex == 0) {
+                if(viewID == R.id.f1Key) {
+                    msg = "{F1}";
+                } else if(viewID == R.id.f2Key) {
+                    msg = "{F2}";
+                } else if(viewID == R.id.f3Key) {
+                    msg = "{F3}";
+                } else if(viewID == R.id.f4Key) {
+                    msg = "{F4}";
+                } else if(viewID == R.id.f5Key) {
+                    msg = "{F5}";
+                } else if(viewID == R.id.f6Key) {
+                    msg = "{F6}";
+                } else if(viewID == R.id.f7Key) {
+                    msg = "{F7}";
+                } else if(viewID == R.id.f8Key) {
+                    msg = "{F8}";
+                } else if(viewID == R.id.f9Key) {
+                    msg = "{F9}";
+                } else if(viewID == R.id.f10Key) {
+                    msg = "{F10}";
+                } else if(viewID == R.id.f11Key) {
+                    msg = "{F11}";
+                } else if(viewID == R.id.f12Key) {
+                    msg = "{F12}";
+                }
+            } else if (currentPageIndex == 1) {
+                if(viewID == R.id.soundUpKey) {
+                    msg = "SOUND UP";
+                } else if(viewID == R.id.soundDownKey) {
+                    msg = "SOUND DOWN";
+                } else if(viewID == R.id.muteKey) {
+                    msg = "SOUND MUTE";
+                } else if(viewID == R.id.tabKey) {
+                    msg = "{TAB}";
+                } else if(viewID == R.id.upKey) {
+                    msg = "{UP}";
+                } else if(viewID == R.id.escKey) {
+                    msg = "{ESC}";
+                } else if(viewID == R.id.insertKey) {
+                    msg = "{INS}";
+                } else if(viewID == R.id.deleteKey) {
+                    msg = "{DEL}";
+                } else if(viewID == R.id.printScreenKey) {
+                    msg = "{PRTSC}";
+                } else if(viewID == R.id.leftKey) {
+                    msg = "{LEFT}";
+                } else if(viewID == R.id.downKey) {
+                    msg = "{DOWN}";
+                } else if(viewID == R.id.rightKey) {
+                    msg = "{RIGHT}";
+                }
+            }
         }
+
         if(!msg.isEmpty()) {
             MainActivity.connectionManager.sendHostMessage("KEYBOARD_WRITE " + msg);
+            Log.d("KeyboardToolbar", "KEYBOARD_WRITE " + msg);
         }
     }
 
