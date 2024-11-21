276a05ff51145051198668180af7c67db21c4d50
Domenic Zarza <dzarza2003@gmail.com>
2024-11-21 17:07:40 -0500

    Implemented unit test 2 for IsClient method.

--- Domenic_Zarza_Homework_5/UnitTest1.cs
+++ Domenic_Zarza_Homework_5/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace Domenic_Zarza_Homework_5
         public void ExtractName()
         {
             var obj = new UDPServerHost();
-            string input = "Connection established by xyz";
+            string input = "Connection requested by xyz";
             string expected = "xyz";
             string actual = obj.ExtractDeviceName(input);
             Assert.Equal(expected, actual);
@@ -17,7 +17,11 @@ namespace Domenic_Zarza_Homework_5
         [Fact]
         public void ClientCheck()
         {
-
+            var obj1 = new UDPServerHost();
+            string ip = "123";
+            bool expected = false;
+            bool actual = obj1.IsClient(ip);
+            Assert.Equal(expected, actual);
         }
     }
 }
