6ea53922a92d7fdf06b8d98bc6d6500c6e007fa8
Fahim-zzz <fahimzaman@wayne.edu>
2024-11-30 11:56:00 -0500

    Added App Unit Testing Logs & Removed Redundant Line in Gradle WorkFlow (#121)
    * Added logging to better see the testing results for app unit testing.
    * Removed testing command after build given building gradle also tests, therefore it was redundant.

--- DAIRemoteApp/app/build.gradle.kts
+++ DAIRemoteApp/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+
 plugins {
     alias(libs.plugins.android.application)
     id("de.mannodermaus.android-junit5") version "1.11.2.0"
@@ -44,4 +46,55 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     testImplementation("org.mockito:mockito-core:5.14.2")
+}
+
+// Test Logging
+tasks.withType<Test> {
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events("started", "skipped", "passed", "failed")
+        showStandardStreams = true
+    }
+
+    var totalTests = 0
+    var passedTests = 0
+    var failedTests = 0
+    var skippedTests = 0
+
+    // Track test statuses
+    addTestListener(object : TestListener {
+        override fun beforeTest(testDescriptor: TestDescriptor) {
+            // Do nothing
+        }
+
+        override fun afterTest(testDescriptor: TestDescriptor, result: TestResult) {
+            totalTests++
+            when (result.resultType) {
+                TestResult.ResultType.SUCCESS -> passedTests++
+                TestResult.ResultType.FAILURE -> failedTests++
+                TestResult.ResultType.SKIPPED -> skippedTests++
+            }
+        }
+
+        override fun beforeSuite(suite: TestDescriptor) {
+            // Do nothing
+        }
+
+        override fun afterSuite(suite: TestDescriptor, result: TestResult) {
+            if (suite.parent == null) {
+                println(
+                    """
+                    -------------
+                    Test Summary:
+                    -------------
+                    Total: $totalTests
+                    Passed: $passedTests
+                    Failed: $failedTests
+                    Skipped: $skippedTests
+                    -------------
+                    """.trimIndent()
+                )
+            }
+        }
+    })
 }
