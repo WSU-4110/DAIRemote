106ad29192441801e85f35b25883a77efd843b1a
Fahim-zzz <fahimzaman@wayne.edu>
2024-12-01 23:58:25 -0500

    Modified project properties to allow publisshing project as an exe with dll dependencies. (#127)
    * Modified Project Properties to Publish Single EXE.

--- UDPServerManager/Program.cs
+++ UDPServerManager/Program.cs
@@ -10,7 +10,7 @@ namespace UDPServerManagerForm
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
+            //ApplicationConfiguration.Initialize();
             Application.Run(new UDPServerManagerForm());
         }
     }
